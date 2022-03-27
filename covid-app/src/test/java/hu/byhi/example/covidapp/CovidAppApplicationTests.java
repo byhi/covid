709a3e589cef8d2ad0924e4b1336153457c2e73b
Zoltán Bihari <byhisoft@gmail.com>
2022-03-27 18:51:49 +0200

    Init project and add covid-app module

---
+++ covid-app/src/test/java/hu/byhi/example/covidapp/CovidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.byhi.example.covidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
